8aeda6703207d8b91c982bef3260fac8f52c414c
Phon1209 <ptphon@gmail.com>
2022-06-24 11:53:28 -0400

    Remove time logger middleware

--- functions/routes/router.js
+++ functions/routes/router.js
@@ -1,11 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-router.use((req, res, next) => {
-  console.log("Time: ", Date.now());
-  next();
-});
-
 router.get("/", (req, res) => {
   res.json("Welcome to CWExpress API home page");
 });
