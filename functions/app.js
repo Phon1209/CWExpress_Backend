dcd50de923ee9b33f83f81d697c8fe5d8854a63a
Phon1209 <ptphon@gmail.com>
2022-07-19 02:32:58 -0400

    install compression
    - to fix express-sse bug of res.flush not a function
    - add middleware in the app.js

--- functions/app.js
+++ functions/app.js
@@ -4,9 +4,11 @@ const router = require("./routes/router");
 const connectDatabase = require("./database/connect");
 const { initializeClient } = require("./utils/mqtt");
 const cors = require("cors");
+const compression = require("compression");
 
 const app = express();
 
+app.use(compression());
 app.use(cors());
 
 app.use(express.json());
