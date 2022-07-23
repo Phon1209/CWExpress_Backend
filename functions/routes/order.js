e705be7306d3de71159779be873d694280cef770
Phon1209 <ptphon@gmail.com>
2022-07-23 02:22:33 -0400

    Add polling route for payment checking

--- functions/routes/order.js
+++ functions/routes/order.js
@@ -8,7 +8,7 @@ router.get("/history", async (req, res) => {
   try {
     const orders = await Order.find()
       .sort("createdAt")
-      .select("status amount createdAt -_id");
+      .select("status amount createdAt ref1 ref2 machineID -_id");
     return res.json(orders);
   } catch (err) {
     res.status(500).json({ error: err });
@@ -22,6 +22,26 @@ router.post("/cleanup", async (req, res) => {
   try {
     const deleteInformation = await Order.deleteMany({ status: "ACTIVE" });
     res.json(deleteInformation);
+  } catch (err) {
+    return res.status(500).json({ error: err });
+  }
+});
+
+// @route   POST  /order/check
+// @desc    check whether the order has been fulfilled
+// @access  Public
+router.post("/check", async (req, res) => {
+  try {
+    const { ref1, ref2, machineID } = req.body;
+    const requestedOrder = await Order.findOne({
+      status: "COMPLETED",
+      ref1,
+      ref2,
+      machineID,
+    }).select("amount fulfilledAt ref1 ref2 transactionID -_id");
+
+    const status = requestedOrder ? 200 : 204;
+    return res.status(status).json(requestedOrder);
   } catch (err) {
     return res.status(500).json({ error: err });
   }
